4c8992176eb9c9f70b86fe592a845cc984e0f0b9
Vinod <ramdinv@gmail.com>
2018-06-15 00:57:19 -0500

    getting data from user

--- app/Data/friends.js
+++ app/Data/friends.js
@@ -1,12 +1,62 @@
- 
+
 var friendArray = [
     {
-        customerName: "Mike",
-        photo: 40,
-        
-        
+        customerName: "Mike Jones",
+        photo: "www.yahoo.com/me/photos",
+        q1: 2,
+        q2: 4,
+        q3: 5,
+        q4: 3,
+        q5: 1,
+        q6: 5,
+        q7: 2,
+        q8: 2,
+        q9: 4,
+        q10: 3
+    }, {
+        customerName: "Vinod Ramdin",
+        photo: "www.photo.com/photo/recent",
+        q2: 2,
+        q1: 1,
+        q3: 2,
+        q4: 2,
+        q5: 4,
+        q6: 3,
+        q7: 3,
+        q8: 3,
+        q9: 4,
+        q10: 4
+    },
+    {
+        customerName: "Joe Johnson",
+        photo: "www.myspace.com/pics/recent",
+        q1: 1,
+        q2: 5,
+        q3: 2,
+        q4: 2,
+        q5: 5,
+        q6: 3,
+        q7: 5,
+        q8: 1,
+        q9: 2,
+        q10: 3
+    },
+    {
+        customerName: "Rajee Bhagaram",
+        photo: "www.myspace.com/pics/recent",
+        q1: 1,
+        q2: 5,
+        q3: 5,
+        q4: 2,
+        q5: 5,
+        q6: 2,
+        q7: 3,
+        q8: 1,
+        q9: 2,
+        q10:3
     }
 ];
+
 
 
 module.exports = friendArray;
--- app/Routing/apiRoutes.js
+++ app/Routing/apiRoutes.js
@@ -6,18 +6,47 @@ module.exports = function (app) {
 
   app.get("/api/friends", function (req, res) {
     res.json(friendArray);
-    console.log(req.body, "<--req.body on apiRoutes");
+
   });
 
   app.post("/api/friends", function (req, res) {
-    
-
-
-
     friendArray.push(req.body);
-    console.log(friendArray[0].customerName);
-    console.log(friendArray[0].photo);
-    console.log(friendArray[1].customerName);
-    console.log(friendArray[1].photo);
-    
-})};
+    userInput(req.body);
+
+  })
+};
+function userInput(userIpt) {
+  console.log(userIpt, "fullresults");
+var userAns = [];
+  // console.log( parseInt(userIpt.q1));
+
+for(let i =0 ; i < 1; i++) {
+  userAns.push(userIpt.q1);
+  userAns.push(userIpt.q2);
+  userAns.push(userIpt.q3);
+  userAns.push(userIpt.q4);
+  userAns.push(userIpt.q5);
+  userAns.push(userIpt.q6);
+  userAns.push(userIpt.q7);
+  userAns.push(userIpt.q8);
+  userAns.push(userIpt.q9);
+  userAns.push(userIpt.q10);
+
+ }
+}
+
+
+var aa = friendArray
+// friendArray[i].q2;
+// friendArray[i].q3;
+// friendArray[i].q4;
+// friendArray[i].q5;
+// friendArray[i].q6;
+// friendArray[i].q7;
+// friendArray[i].q8;
+// friendArray[i].q9;
+// friendArray[i].q10;
+
+
+ 
+ console.log(aa);
