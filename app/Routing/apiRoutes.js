cab0ce6687132bb2a7fa3ba5b58321b1f7069bc6
Vinod <ramdinv@gmail.com>
2018-06-18 14:12:14 -0500

    added some more info

--- app/Routing/apiRoutes.js
+++ app/Routing/apiRoutes.js
@@ -20,6 +20,8 @@ var userAns = [];
 var preFrdOne = [];
 var difference = [];
 var results = []; 
+var a = preFriends;
+var ordNum = 0
 
 function userInput(userIpt) {
   for (let i = 0; i < 1; i++) {
@@ -34,22 +36,22 @@ function userInput(userIpt) {
     userAns.push(userIpt.q9);
     userAns.push(userIpt.q10);
   }
-  frdChooser();
+  frdChooser(ordNum);
 }
-var a = preFriends;
-function frdChooser() {
-  for (let i = 0; i < 1; i++) {
-    preFrdOne.push(a[i].q1);
-    preFrdOne.push(a[i].q2);
-    preFrdOne.push(a[i].q3);
-    preFrdOne.push(a[i].q4);
-    preFrdOne.push(a[i].q5);
-    preFrdOne.push(a[i].q6);
-    preFrdOne.push(a[i].q7);
-    preFrdOne.push(a[i].q8);
-    preFrdOne.push(a[i].q9);
-    preFrdOne.push(a[i].q10); 
-  }
+
+function frdChooser(n) {
+   
+    preFrdOne.push(a[n].q1);
+    preFrdOne.push(a[n].q2);
+    preFrdOne.push(a[n].q3);
+    preFrdOne.push(a[n].q4);
+    preFrdOne.push(a[n].q5);
+    preFrdOne.push(a[n].q6);
+    preFrdOne.push(a[n].q7);
+    preFrdOne.push(a[n].q8);
+    preFrdOne.push(a[n].q9);
+    preFrdOne.push(a[n].q10); 
+  
   totalDiff();
 }
 
@@ -67,13 +69,16 @@ var num = 0;
 for(var t = 0; t < difference.length; t++) {
     num = num + difference[t];
 }
-  results.push(num) ;
+  results.push(num);
+   preFrdOne = [];
+   difference = [];
+  frdChooser(1);
   console.log(results);
 }
 
 
-
-
+//call the frdChooser pass arg of 1 then 2 then 3...
+//after eache prefriend reset back arrays so next prefriend can be calculated. 
 
 
 
